679a55abd553b09be3b5a64229a57b9eb7b2df88
atsumu22 <w.atsumu27@gmail.com>
2023-06-05 16:10:40 +0900

    set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"
+  resources :users, only: %i[edit update destroy]
+  resources :sessions do
+    resources :reviews,  only: %i[new create]
+    resources :session_tracks,  only: %i[new creat]
+  end
+  resources :karaoke_hubs
+  resources :tracks #showing the list of tracks for browsing with using AJAX? still any of track instanses isn't created yet.
+  resources :reviews,  only: %i[edit update destroy]
+  resources :session_tracks,  only: %i[edit update destroy]
+  get "pages#search" #searching tracks over API on this action
+  get "pages#associate" #asking API any recommendation on this action
 end
